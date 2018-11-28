92cd1aa1d37b0ecfd621ea9e6e703ed13f9bdce5
Geco <geco.hh@gmail.com>
2018-11-28 11:52:00 +0000

    added a loading spinner

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <header class="card-header">
         <h2 class="title is-2 is-medium has-text-info">Caluladora de swag</h2>
       </header>
-    <the-swag-form @show-swag="showSwag($event)"/>
-    <the-swag-score v-if="totalSwag>0" :score="totalSwag"/>
+      <the-swag-form @show-swag="showSwag($event)"/>
+      <the-swag-score v-if="totalSwag>0" :score="totalSwag"/>
     </div>
   </div>
 </template>
@@ -29,6 +29,7 @@ export default {
   methods: {
     showSwag(totalSwag) {
       this.totalSwag = totalSwag
+
     },
   },
 
@@ -44,9 +45,11 @@ export default {
   body {
     padding-top: 50px;
   }
+
   div.card {
     max-width: 720px;
   }
+
   header {
     padding-left: 20px;
     display: block;
--- src/the-swag-score.vue
+++ src/the-swag-score.vue
@@ -1,30 +1,74 @@
 <template>
-    <div>
-        hola {{ score }}
-    </div>    
+    <footer class="card-footer">
+        <div v-if="isLoadingSwag" class="card-footer-item">
+            <the-loader/> 
+            <p class="padding-left-20"> {{ currentMessage }} </p>
+        </div>
+        <div v-else class="card-footer-item"> 
+            <p> Puntuación final: <span class="has-text-info">{{ score }}</span> swags</p>
+        </div>
+    </footer>    
+
 </template>
 
 
 
 <script>
+import TheLoader from './loader.vue'
+
 export default {
 
     data() {
         return {
+            messages: [ "Consultado bases de datos mundiales de swag",
+                        "Haciendo análisis de características complejas",
+                        "Implementando modelos de deep learning",
+                        "Intendando eliminar sexismo en el modelo de regresión",
+                        "Intento de extracción fallido, arreglando resultados",
+                        "Pausa dramática" ],
+            currentMessageIndex: 0,
+            isLoadingSwag:  false,
             
-
-
         }
     },
 
-    props: ['score'],
+    computed: {
+
+        currentMessage() {
+            return this.messages[this.currentMessageIndex]
+        },
+    },
+
+    components: { TheLoader },
+
+    props: [ 'score' ],
 
     mounted() {
-        /** First time the requirements for this to display are met */
-        /** Beauitful, intrusive, over the top loading animation goes here  */
 
-        console.log("mounted")
+        this.startTimeOut()
+
     },
+
+    methods: {
+
+        startTimeOut(){
+            // sets the spinner
+            this.isLoadingSwag = true 
+
+            var interval = setInterval(() => {
+                // rolls every message at a fixed interval until the  last one is reached
+                this.currentMessageIndex++
+                if ( this.currentMessageIndex >= this.messages.length ) {
+                  clearInterval(interval)
+                  this.isLoadingSwag = false
+                }
+
+            }, 2000);
+
+        },
+
+
+    }
 
 }
 
@@ -35,7 +79,9 @@ export default {
 
 
 <style>
-    
+    .padding-left-20 {
+        padding-left: 20px;
+    }
 </style>
 
 
